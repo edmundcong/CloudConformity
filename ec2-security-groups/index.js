8f6cd8dd66c18f991c646cd8eef03c50cd9fc562
edmundcong <eddy9412@gmail.com>
2019-07-09 17:42:33 +1000

    got module working

--- ec2-security-groups/index.js
+++ ec2-security-groups/index.js
@@ -9,16 +9,18 @@ AWS.config.update({region: 'us-west-2'});
  * @param secGroupArr SecurityGroup object's array of SecurityGroups
  * @returns {String[]}
  */
-exports.constructSecurityGroupsArray = (secGroupArr) => secGroupArr.map(secGroup => {
+const constructSecurityGroupsArray = (secGroupArr) => secGroupArr.map(secGroup => {
     return { id: secGroup.GroupId, type: "GroupObject", attributes: { GroupName: secGroup.GroupName }}
 });
 
-exports.fetchSecurityGroups = async () => {
+const fetchSecurityGroups = async () => {
     // Create EC2 service object
     const ec2 = new AWS.EC2({apiVersion: '2016-11-15'});
     // Retrieve security group descriptions. Note that this returns an AWS.request object (not a promise)
     const ec2GroupsRequests = ec2.describeSecurityGroups({});
     // await thennable promise of describeSecurityGroup request
     const ec2DataPromise = await (ec2GroupsRequests.promise());
-    return ec2DataPromise.SecurityGroups;
-}
+    return ec2DataPromise;
+};
+
+module.exports = {constructSecurityGroupsArray, fetchSecurityGroups}
--- get-security-groups/index.js
+++ get-security-groups/index.js
@@ -1,10 +1,12 @@
+const {fetchSecurityGroups, constructSecurityGroupsArray} = require('ec2-security-groups');
+
 exports.handler = async (event, ctx) => {
     try {
         // Get awsRequestId and use as ID for json-api response via destructuring
         const {awsRequestId} = ctx;
-        let data = await fetchSecurityGroups();
+        let data = (await fetchSecurityGroups()).SecurityGroups;
         data = constructSecurityGroupsArray(data);
-        if (ec2DataPromise.length == 0) {
+        if (data.length === 0) {
             return {id: awsRequestId, data: [], type: "array"}
         } else {
             return {id: awsRequestId, data, type: "array"};
