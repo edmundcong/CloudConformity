54500b123332d4ee768c5f011cc2f1f6b750666a
edmundcong <eddy9412@gmail.com>
2019-07-09 19:35:25 +1000

    implemented request param checking

--- get-security-groups/index.js
+++ get-security-groups/index.js
@@ -1,30 +1,46 @@
 const {fetchSecurityGroups, constructSecurityGroupsArray} = require('ec2-security-groups');
 
 exports.handler = async (event, ctx) => {
+    // Get awsRequestId and use as ID for json-api response via destructuring
+    const {awsRequestId} = ctx;
+    const jsonApiHeaders = {"Content-Type": "application/vnd.api+json"};
+
+    // client's requests need to specify the content type, and that content type needs to be vnd.api+json
+    if (!event.headers["Content-Type"] || event.headers["Content-Type"] !== "application/vnd.api+json") {
+        return {
+            statusCode: "406", headers: jsonApiHeaders,
+            body: JSON.stringify({errors: [{status: "415", detail: "Unsupported Media Type"}]})
+        };
+    }
+    // if a client does a request with an accept header, make sure they're looking to get returned vnd.api+json
+    if (event.headers["Accept"] && event.headers["Accept"] !== "application/vnd.api+json") {
+        return {
+            statusCode: "406", headers: jsonApiHeaders,
+            body: JSON.stringify({errors: [{status: "406", detail: "Not Acceptable"}]})
+        };
+    }
     try {
-        // Get awsRequestId and use as ID for json-api response via destructuring
-        const {awsRequestId} = ctx;
         let data = (await fetchSecurityGroups()).SecurityGroups;
         data = constructSecurityGroupsArray(data);
         if (data.length === 0) {
             return {
                 statusCode: 200,
-                headers: {"Content-Type": "application/vnd.api+json"},
+                headers: jsonApiHeaders,
                 body: []
             }
         } else {
             return {
                 statusCode: 200,
-                headers: {"Content-Type": "application/vnd.api+json"},
+                headers: jsonApiHeaders,
                 body: JSON.stringify({id: awsRequestId, data, type: "array"})
             }
         }
     } catch (error) {
         const statusCode = error.code || "500";
-        const detail  = error.message || "Internal Server Error.";
+        const detail = error.message || "Internal Server Error.";
         return {
             statusCode,
-            headers: {"Content-Type": "application/vnd.api+json"},
+            headers: jsonApiHeaders,
             body: JSON.stringify({errors: [{status: statusCode, detail}]})
         }
     }
