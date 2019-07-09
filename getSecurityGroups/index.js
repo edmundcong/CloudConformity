2ec3e533e4b7f687b54e9ac26394add41e5f0edd
edmundcong <eddy9412@gmail.com>
2019-07-09 13:32:25 +1000

    looks like sec groups are being returned.

--- getSecurityGroups/index.js
+++ getSecurityGroups/index.js
@@ -1,8 +1,23 @@
+// load SDK
+const AWS = require('aws-sdk');
+// set region
+AWS.config.update({region: 'us-west-2'});
+
 exports.handler = async (event) => {
-    // TODO implement
+    // Create EC2 service object
+    var ec2 = new AWS.EC2({apiVersion: '2016-11-15'});
+    // Retrieve security group descriptions
+    const ec2Data = await ec2.describeSecurityGroups({}, (err, data) => {
+        if (err) {
+            return err;
+        } else {
+            return data.SecurityGroups;
+        }
+    });
+    const ec2DataPromise = await (ec2Data.promise());
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
+        body: JSON.stringify(ec2DataPromise),
     };
     return response;
 };
