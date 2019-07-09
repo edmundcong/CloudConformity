52bce26ed50c2e9a78b3db83bc1a712fb2836a6e
edmundcong <eddy9412@gmail.com>
2019-07-09 15:32:06 +1000

    returning array of strings

--- getSecurityGroups/index.js
+++ getSecurityGroups/index.js
@@ -3,21 +3,22 @@ const AWS = require('aws-sdk');
 // set region
 AWS.config.update({region: 'us-west-2'});
 
+const constructSecurityGroupsArray = (secGroupArr) => secGroupArr.map(e => e.GroupName);
+
 exports.handler = async (event) => {
-    // Create EC2 service object
-    var ec2 = new AWS.EC2({apiVersion: '2016-11-15'});
-    // Retrieve security group descriptions
-    const ec2Data = await ec2.describeSecurityGroups({}, (err, data) => {
-        if (err) {
-            return err;
+    try {
+        // Create EC2 service object
+        var ec2 = new AWS.EC2({apiVersion: '2016-11-15'});
+        // Retrieve security group descriptions. Note that this returns an AWS.request object (not a promise)
+        const ec2GroupsRequests = ec2.describeSecurityGroups({});
+        // await thennable promise of describeSecurityGroup request
+        const ec2DataPromise = await (ec2GroupsRequests.promise());
+        if (ec2DataPromise.length == 0) {
+            return {data: []}
         } else {
-            return data.SecurityGroups;
+            return {data: constructSecurityGroupsArray(ec2DataPromise.SecurityGroups)};
         }
-    });
-    const ec2DataPromise = await (ec2Data.promise());
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify(ec2DataPromise),
-    };
-    return response;
+    } catch (error) {
+        return {error: "Internal Server Error"};
+    }
 };
